129489f50aa8015d7595b55a065b0dedcfdd65de
Horacio Mota <horaciomota93@gmail.com>
2023-09-09 12:33:55 +0100

    First commit

---
+++ TabNewsSwift/Model/NewsModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewsModel.swift
+//  TabNewsSwift
+//
+//  Created by Horacio Mota on 09/09/2023.
+//
+
+import Foundation
---
+++ TabNewsSwift/View/ListOfNewsView.swift
@@ -0,0 +1,20 @@
+//
+//  ListOfNewsView.swift
+//  TabNewsSwift
+//
+//  Created by Horacio Mota on 09/09/2023.
+//
+
+import SwiftUI
+
+struct ListOfNewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListOfNewsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListOfNewsView()
+    }
+}
